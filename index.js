41e06b10d88863fc310875d6f9c31dc35a889cb2
kaelzhang <i@kael.me>
2014-04-07 23:27:11 +0800

    basic functionalities of rule pattern

--- index.js
+++ index.js
@@ -1,24 +1,107 @@
-
-// Opt in to strict mode of JavaScript, [ref](http://is.gd/3Bg9QR)
-// Use this statement, you can stay away from several frequent mistakes 
 'use strict';
 
-// How to use a foreign module ?
-// Take 'sample-cortex-project' for example:
-//
-// 1. to install a dependency, exec the command below inside the current repo:
-// 		cortex install sample-cortex-project --save
-// 2. use `require(module_idendifier)` method:
-// 		var hello = require('sample-cortex-project');
+module.exports = router;
+router.Rule = Rule;
+router.Router = Router;
 
-// `exports` is the API of the current module
-exports.my_method = function() {
-    // your code...
+function router (options) {
+    return new Router(options);
+}
+
+
+var REPLACES = [
+    [
+        // Some chars need to be escaped or 
+        // they will be transcribed into key words when `new RegExp`
+        /[\-{}\[\]+?.,\\\^$|#\s]/g, 
+        '\\$&'
+    ],
+
+    [
+        // Ungroup the optional group, 
+        // so the matched parts of route patterns should not be ruined 
+        // '(abc)' -> '(?:abc)?'
+        /\((.*?)\)/g,
+        '(?:$1)?'
+    ],
+
+    [
+        // Named parameters
+        /(:|\*)(\w+)/g,
+
+        // @this {Rule}
+        function (match, type, name) {
+            this.keys.push(name);
+
+            return type === ':'
+                ? '([^\/?]+?)'
+                : '([^?]+?)';
+        }
+    ]
+
+    // .replace(REGEX_NAMED_PARAM, function(match, optional) {
+    //     return optional ? match : '([^/?]+)';
+    // })
+    // .replace(splatParam, '([^?]*?)');
+];
+
+
+// The first group was what `REGEX_OPTIONAL_GROUP` replaced with.\
+// '/(:city)?' -> '/(?::city)?'
+// var REGEX_NAMED_PARAM       = /(\(\?)?:\w+/g;
+
+// var REGEX_    = /\*\w+/g; 
+
+
+function Rule (pattern) {
+    this.pattern = pattern;
+    this.keys = [];
+
+    this._makeRegex();
+}
+
+
+Rule.prototype._makeRegex = function() {
+    var route = this.pattern;
+
+    REPLACES.forEach(function (replace) {
+        var replacer = replace[1];
+
+        if ( typeof replacer === 'function' ) {
+            replacer = replacer.bind(this);
+        }
+
+        route = route.replace(replace[0], replacer);
+    }, this);
+
+    this.regex = new RegExp('^' + route + '(?:\\?(.*))?$');
 };
 
-// or you could code like this:
-// 		module.exports = {
-// 			my_method: function() {
-// 	    		hello();
-// 			}
-// 		};
+
+Rule.prototype.match = function(path) {
+    var match = path.match(this.regex);
+
+    return match && this._makeObject(match);
+};
+
+
+Rule.prototype._makeObject = function(match) {
+    var obj = {};
+
+    this.keys.forEach(function (key, index) {
+        obj[key] = match[index + 1];
+    });
+
+    return obj;
+};
+
+
+function Router (options) {
+    
+}
+
+
+
+
+
+
